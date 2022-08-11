6cfbd9e66419ac7ad1c2092cf3bf2c05ca72adaa
Elena <p0zhar@yandex.ru>
2022-08-11 10:55:25 +0400

    домашнее задание 1

---
+++ DZ10/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+Console.WriteLine("Вводим любое трехзначное число:");
+string? numberString = Console.ReadLine ();
+var number=  int.Parse (numberString!);
+
+//Random random = new Random();   //- для случайных чисел
+//var randomNumber = random.Next(100, 1000); //- для случайных чисел
+//Console.WriteLine(randomNumber);  // - для случайных чисел
+//var number = (randomNumber % 100) / 10;  // - для случайных чисел
+
+number = (number % 100) / 10;
+Console.WriteLine("Выводим вторую цифру этого числа:");
+Console.WriteLine(number);
